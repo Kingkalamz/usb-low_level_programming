b2a84ee202e403474994c8561fc0a592dbdba0e9
Kingkalamz <musaogunsolu@gmail.com>
2023-04-04 14:38:09 -0700

    practice

---
+++ 00x00-hello_world/3-math_operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//This tasks involves the usage of four different arithmetic operators between 5 & 2.
+
+int main()
+{
+	printf("%d %d %d\n", 5, 2, 5+2);
+	printf("%d + %d = %d\n", 5, 2, 5+2);
+	printf("%d - %d = %d\n", 5, 2,  5-2);
+	printf("%d * %d = %d\n", 5, 2, 5*2);
+	printf("%d / %d = %d\n", 5, 2, 5/2);
+	return 0;
+}
